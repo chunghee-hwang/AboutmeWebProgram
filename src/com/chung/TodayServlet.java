59553f22f77865a0969213a16b6077849aa78cc9
hch0821 <hch0821@gmail.com>
2019-09-27 15:21:43 +0900

    Update TodayServlet.java

--- src/com/chung/TodayServlet.java
+++ src/com/chung/TodayServlet.java
@@ -27,7 +27,6 @@ public class TodayServlet extends HttpServlet {
 		out.println("</head>");
 		
 		//수직 정렬 방법 출처 : https://aboooks.tistory.com/180
-		
 		out.println("<body style='display:table; width:100%;height:100%;'>");
 		out.println("<a style='position:absolute; top:0;left:0;' href='html/index.html'><h2>메인 화면</h2></a><br>");
 		out.println("<h1 style='display:table-cell; vertical-align:middle; text-align:center;'>" + today + "</h1>");
