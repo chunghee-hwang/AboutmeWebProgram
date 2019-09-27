15ae8f7038ce8dc65adab0b41f3a7ccc92464b2e
hch0821 <hch0821@gmail.com>
2019-09-27 15:21:12 +0900

    Update TodayServlet.java

--- src/com/chung/TodayServlet.java
+++ src/com/chung/TodayServlet.java
@@ -11,24 +11,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 @WebServlet("/today")
 public class TodayServlet extends HttpServlet {
-     
-    @Override
+
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String today = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 a hh시 mm분"));
-		
+
 		response.setContentType("text/html;charset=UTF-8");
-		
+
 		PrintWriter out = response.getWriter();
 		out.println("<html>");
 		out.println("<head>");
 		out.println("<title>몇 시인가요</title>");
 		out.println("</head>");
+		
+		//수직 정렬 방법 출처 : https://aboooks.tistory.com/180
+		
 		out.println("<body style='display:table; width:100%;height:100%;'>");
 		out.println("<a style='position:absolute; top:0;left:0;' href='html/index.html'><h2>메인 화면</h2></a><br>");
-		out.println("<h1 style='display:table-cell; vertical-align:middle; text-align:center;'>"+today+"</h1>");
+		out.println("<h1 style='display:table-cell; vertical-align:middle; text-align:center;'>" + today + "</h1>");
 
 		out.println("</body>");
 		out.println("</html>");
